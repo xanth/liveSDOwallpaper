6a5667ce5b5d6e1980ff6b7750b1ecc8bfd0fdec
Alex Cruice <alex.cruice@gmail.com>
2014-08-13 21:21:49 +1000

    minor refactor

--- imagePull.rb
+++ imagePull.rb
@@ -3,25 +3,14 @@
 require 'open-uri'
 
 def internet?
-  	begin
-    		true if open("http://www.google.com/")
-  	rescue
-    		false
-  	end
+  open('http://www.google.com/') ? true : false
 end
 
 if internet?
-	t = Time.now.utc
-
-	begin
-		File.delete('theSun.jpg')
-	rescue
-	
-	end 
-
-	open('theSun.jpg', 'wb') do |file|
-  		file << open("http://sdowww.lmsal.com/sdomedia/SunInTime/#{t.year}/#{"%.2d" % t.month}/#{"%.2d" % t.day}/f_211_193_171.jpg").read
-	end
-
-	system( "bash imageSet.sh" )
+  t = Time.now.utc
+  remote_sun_file = "http://sdowww.lmsal.com/sdomedia/SunInTime/#{t.year}/#{format('%.2d', t.month)}/#{format('%.2d', t.day)}/f_211_193_171.jpg"
+  local_sun_file = 'theSun.jpg'
+  File.delete local_sun_file if File.exist? local_sun_file
+  open(local_sun_file, 'wb') { |file| file << open(remote_sun_file).read }
+  system 'bash imageSet.sh'
 end
