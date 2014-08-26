6b438fc0e94191d8572ae59d74a4e49b7a6eb053
Joshua Head <joshua.head@outlook.com>
2014-08-26 19:48:14 +1000

    Migrated to httparty

--- imagePull.rb
+++ imagePull.rb
@@ -1,19 +1,28 @@
 #!/usr/bin/env ruby
 # Changes wallpaper to the most up to date SDO image; http://sdowww.lmsal.com/suntoday/#
-require 'open-uri'
+require 'httparty'
 
-def internet?
-  open('http://www.google.com/') ? true : false
-end
-
-if internet?
-  
-  t = Time.now.utc
+def f(res)
   installLocation = File.dirname(File.expand_path $0)
-  remote_sun_file = "http://sdowww.lmsal.com/sdomedia/SunInTime/#{t.year}/#{format('%.2d', t.month)}/#{format('%.2d', t.day)}/f_211_193_171.jpg"
   local_sun_file = "#{installLocation}/theSun.jpg"
 
   File.delete local_sun_file if File.exist? local_sun_file
-  open(local_sun_file, 'wb') { |file| file << open(remote_sun_file).read }
-  system "bash #{installLocation}/imageSet.sh #{installLocation}"
+    
+  File.open(local_sun_file, "wb") do |io| 
+    io.write res.parsed_response
+  end
+  
+  # system "bash #{installLocation}/imageSet.sh #{installLocation}"
+end
+
+
+
+t = Time.now.utc
+
+remote_sun_file = "http://sdowww.lmsal.com/sdomedia/SunInTime/#{t.year}/#{format('%.2d', t.month)}/#{format('%.2d', t.day)}/f_211_193_171.jpg"
+respone = HTTParty.get(remote_sun_file);
+
+case respone.code
+when 200
+  f(respone)
 end
